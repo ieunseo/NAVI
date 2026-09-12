8b8bf35dc14da8880003a1ac873eb5fc41961760
ieunseo <y949512@naver.com>
2026-09-12 22:35:19 +0900

    #1 - 공통설정

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,43 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
 import { AuthProvider } from "@/providers/AuthProvider";
 
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+    duration: 400,
+    fade: true,
+});
+
 export default function RootLayout() {
-  return (
-      <AuthProvider>
-        <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-        >
-          <Stack.Screen name="index" />
+    useEffect(() => {
+        const prepare = async () => {
+            try {
+                // 나중에 폰트 로딩이나 초기 설정 로딩을 여기서 처리
+            } catch (error) {
+                console.warn(error);
+            } finally {
+                await SplashScreen.hideAsync();
+            }
+        };
 
-          <Stack.Screen name="create" />
+        prepare();
+    }, []);
 
-          <Stack.Screen name="explore" />
-
-          <Stack.Screen
-              name="auth"
-              options={{
-                headerShown: false,
-              }}
-          />
-        </Stack>
-      </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="index" />
+                <Stack.Screen name="create" />
+                <Stack.Screen name="explore" />
+                <Stack.Screen name="auth" />
+            </Stack>
+        </AuthProvider>
+    );
 }
